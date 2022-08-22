5f3dfd0808cdc800e09c07ab47632bc284fd37a5
Cilvya <cilvya.hg@gmail.com>
2022-08-22 15:00:29 +0200

    forEach() on styleproperty

--- DOM/Colt_Dom/coltDomNotes.md
+++ DOM/Colt_Dom/coltDomNotes.md
@@ -5,7 +5,7 @@ The document object is our entry point into the world of the DOM. It contains re
 it's all nested in a tree structure
 document is our entry point that is why we use document. --> do something
 
-console.dir(document). 
+console.dir(document).
 
 ### SELECTING
 
@@ -13,13 +13,50 @@ what the DOM returns it looks like a HTML element, BUT it's not it's an OBJECT
 set it to a variable and you can use the console.dir(object).
 for example a image. then you will get an object returned. It's an object in the JS.
 
-
 **types**
-__proto___: HTMLImageElement
+**proto\_**: HTMLInputElement, HTMLParagraphElement, HTMLCollection and etc.
 there are different categories of types like HTMLImageElement. because every element has their own properties for example a image has a src property while another element hasn't.
 
+you can find one element, save it to a variable search within thst if you need to.
+
+- getElementsByTagName
+- getElementsByClass
+- getElementById
+
+* the above one returns a HTML collection. you can't use the array methods on it.
+
+- querySelector
+
+* a newer, all in one method to slect a single element
+* pass in a CSS selector .. you can get really specific like
+  `document.querySelector('input[type="text"]')`
+  `document.querySelector('ul li.special')` so the li with the class special inside of the ul element.
+* returns a nodeList (so the container they give back is different, but according to colt this is not really a big deal)
+* you can use forEach() on a Nodelist and not on a HTML collection.
+
+## Value, checked
+
+read out the value from input and radiobuttons
+for radiobuttons use radiobutton.checked = true/false
+input.value = " ...."
+range.value = " ... " (set for example a max and min)
 
 
+### href, src
+some attributes like src and href can be directly accessed on the object
+
+## nextElementSibling, previousElementSibling, parentElement, children
+these are properties. 
+
+## Style property
+every element has a style property.
+if you set something with the style property you style it inline so it will be in the HTML page. 
+disadvantage ; is that you'll have to do every style seperately that is why this is not convenient. Style one at a time. so it's better to use classed in the CSS.
+property names of style properties are camelCased there are no hyphens
+
+
+!NOTE
+because inline styles do have priority over css styling that is why those styles will be applied when style.property is used in javascript. so it's not changing the style in our CSS but changing the styles in our inline styling individually.
 
 
 
--- DOM/Colt_Dom/script.js
+++ DOM/Colt_Dom/script.js
@@ -1,3 +1,112 @@
 'use strict';
 
-const log = console.log
+const log = console.log;
+
+// const inputAll = document.getElementsByTagName('input');
+// log(inputAll); // HTML collection
+
+// const inputValue = function () {
+//   for (let input of inputAll) {
+//     log(input.value);
+//   }
+// };
+
+const list1 = document.getElementsByTagName('ul')[0];
+
+log(list1, `I am the first child::`, list1.firstElementChild);
+
+log((list1.children[2].innerText = 'I am modified numero 2'));
+const list1Children = list1.children;
+log(list1Children);
+// or (to acces the children you need to define the index of the child)
+const nextSibling = list1Children[0].nextElementSibling.nextElementSibling;
+const previousSibling = nextSibling.previousElementSibling;
+log(nextSibling, previousSibling);
+
+log(list1.getElementsByTagName('li')); // here you get all the li items within the UL / returns a htmlcollection
+log(list1.querySelectorAll('li')); // returns a nodelist
+
+const special = list1.getElementsByClassName('special'); // to target it more specifically
+log(special);
+
+log(document.querySelectorAll('li.special')[1]);
+
+log(document.querySelector('section li.special')); //   peas
+
+log(document.querySelector('input[type="password"]'));
+
+// FORM
+
+const inputs = document.querySelectorAll('input');
+log(inputs);
+
+const a = document.querySelector('a');
+log(a);
+
+// attributes that we can enter and modify.
+const img = document.querySelector('img');
+log(img); // butterfly picture
+
+// another butterfly picture. the src attribute can be directly accessed on the object
+img.src =
+  'https://images.unsplash.com/photo-1470107355970-2ace9f20ab15?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGJ1dHRlcmZseXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60';
+
+const range = document.querySelector('input[type="range"]');
+log(range.getAttribute('max')); // looking for the attribute from the object
+
+range.setAttribute('min', '-500');
+log(range.min);
+
+// range.setAttribute('type', 'radio');
+// // changed the range element in a radio.
+
+const firstLi = document.querySelector('li');
+log(firstLi.parentElement); // you can use console.dir(firstLi) to see what the parentElement is
+
+
+// CHANGING MULTIPLE ELEMENTS :: using loops
+
+const allLi = document.querySelectorAll('li');
+log(allLi);
+
+
+for (let li of allLi) {
+  li.innerHTML = 'We <b>love</b> you';
+  log(li);
+}
+
+// ====== style =====
+
+const colores = ["red", "orange", "blue", "purple", "pink", "brown", "teal", "yellow", "green"]
+const coloresReverse = [...colores].reverse(); // making a copy of the array and reverse() method
+coloresReverse[4] = "beige";
+log(coloresReverse)
+
+
+// the forEach accept three parameters so element, index, array;
+// that is why we are using forEach here, and not a for .. of loop because for .. of doesn't accept a index parameter
+
+log(colores.length); // output is 9 
+
+allLi.forEach(function (el, index) {
+  const colorIndex = colores[index];
+  log(colorIndex);
+  el.style.color = colorIndex;
+  el.style.backgroundColor = coloresReverse[index];
+  // el.style.color = colors[index]; // this can also be done
+});
+
+
+const h1 = document.querySelector('h1');
+log(h1.style) 
+// the style property will changes the inline style in html. you can only do it one by one
+h1.style.color = "orchid";
+h1.style.backgroundColor = "beige";
+h1.style.textAlign = "center";
+h1.style.borderRadius = "100px";
+h1.style.padding = "1rem";
+
+
+
+
+

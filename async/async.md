cb244c8a538dbc0634586839e31346df786246ed
Cilvya <cilvya.hg@gmail.com>
2022-08-31 15:37:32 +0200

    js callbacks

--- async/async.md
+++ async/async.md
@@ -1,3 +1,52 @@
+# Javascript is single threaded 
+which means it can only do one thing at a time.
+
+The browser does the work. js is not the same as the browser. js is a language. browser is like firefox and etc. browser is capable of doing things that js can't. The browser takes over and reminds js when it's time to do certain things 
+
+so for example with setTimeOut(function, time) --> js is not settting A TIMER or keeping TRACK of how MANY SECONDS have gone by. 
+javascript is not sending a request to the API, it's the browser that is doing it. it's given to the browser. 
+
+for the setTimeOut() the browser will take the callback function after the amount of seconds has elapsed it will put on the callstack. 
+
+setTimeOut is something the browser will do for Javascript. the browser is setting a timeout and js is free to continu with the code. JS is not keeping the time. JS is not making a request
+
+
+```
+setTimeOut(reference function, time, param1, param2) is a method
+```
+
+```
+const makeHomeWork = (course, callback) => {
+    log(`Okay I am going to do my ${course} homework now`);
+    callback(); 
+  // given to the browser and put it on the callstack after 1 sec.
+};
+
+
+const doneWithHomework = function () {
+  log(`Look mom and dad i'm done with home work`);
+};
+
+
+setTimeout(makeHomeWork, 5000, 'math', doneWithHomework); 
+// if you want to pass in parameters do it after the time ms and not as a invocation of the function//
+
+```
+
+* what you get returned when logging setTimeOut() is the timeOutID. this is the identifier of the timeout you want to cancel. This ID was returned by the corresponding call to setTimeOut()
+
+* the value of an setTimeOut is the timeoutID
+
+## JS Callback
+synchronous ==> one thing at a time .. wait until one task is done
+asynchronous ==> a task is executed but you don't want to wait until it's totally done in order to move to the next task
+
+for example when you want to get your data, but you don't want js to wait until you get your data. 
+
+A callback is a general JavaScript concept: it is simply a function that is only executed after another function is carried out.
+
+
+
 ## LIVE LES ASYNC AWAIT / PROMISES
 
 Promise.all() method
@@ -38,7 +87,7 @@ if you want to use await in your function, your function has to be async.
 await always returns a promise.
 
 
-
+## JS PROMISES 
 
 
 
--- async/script.js
+++ async/script.js
@@ -9,77 +9,155 @@ const log = console.log;
 
 // log('Hey hello, I am the third.');
 
-const BASE_URL = 'https://cat-fact.herokuapp.com/';
+// const BASE_URL = 'https://cat-fact.herokuapp.com/';
 
-const get_random_cat_fact = async () => {
-  // async functie geeft een Promise functie terug.
-  const result = await fetch(`${BASE_URL}facts/random`);
-  log(result); // fetch has the standard method of GET
-  const json = await result.json();
-  log(json);
-  return json.text;
-  // return json.text;
-};
+// const get_random_cat_fact = async () => {
+//   // async functie geeft een Promise functie terug.
+//   const result = await fetch(`${BASE_URL}facts/random`);
+//   log(result); // fetch has the standard method of GET
+//   const json = await result.json();
+//   log(json);
+//   return json.text;
+//   // return json.text;
+// };
 
-log(get_random_cat_fact());
+// log(get_random_cat_fact());
 
 // PROMISES -- PENDING, RESOLVED, REJECTED
 //.then() , .catch() -- pass another callback
 
-const promises = new Promise(function (resolve, reject) {
-  let value = false;
-  if (value) {
-    resolve('Hi the promise is resolved');
-  } else {
-    const a = 'there was an error';
-    reject(a);
-  }
-});
+// const promises = new Promise(function (resolve, reject) {
+//   let value = false;
+//   if (value) {
+//     resolve('Hi the promise is resolved');
+//   } else {
+//     const a = 'there was an error';
+//     reject(a);
+//   }
+// });
 
-promises
-  .then(function (succesful) {
-    return succesful;
-  })
-  .catch(function (error1) {
-    log(`${error1}`);
-  });
+// promises
+//   .then(function (succesful) {
+//     return succesful;
+//   })
+//   .catch(function (error1) {
+//     log(`${error1}`);
+//   });
 
-log(promises);
+// log(promises);
 
 // ========== ASYNC JS =========
 // setTimeOut(function, time) is not a web API thing and not a JS thing
 
-boilWater(1000); // this means minimum time ...
+// boilWater(1000); // this means minimum time ...
 
-for (let i = 0; i < 1000; i++) {
-  log('still busy');
-}
+// for (let i = 0; i < 50; i++) {
+//   log('still busy');
+// }
 
-log('2 chop carrots');
-log('2 chop carrots');
-log('2 chop carrots');
-log('2 chop carrots');
-log('2 chop carrots');
-log('2 chop carrots');
-log('2 chop carrots');
-log('2 chop carrots');
-log('2 chop carrots');
-log('2 chop carrots');
-log('2 chop carrots');
-log('2 chop carrots');
-log('2 chop carrots');
-log('2 chop carrots');
-log('2 chop carrots');
-log('2 chop carrots');
-log('2 chop carrots');
-log('2 chop carrots');
-log('2 chop carrots');
-log('2 chop carrots');
+// log('2 chop carrots');
+// log('2 chop carrots');
+// log('2 chop carrots');
+// log('2 chop carrots');
+// log('2 chop carrots');
+// log('2 chop carrots');
+// log('2 chop carrots');
+// log('2 chop carrots');
+// log('2 chop carrots');
+// log('2 chop carrots');
+// log('2 chop carrots');
+// log('2 chop carrots');
+// log('2 chop carrots');
+// log('2 chop carrots');
+// log('2 chop carrots');
+// log('2 chop carrots');
+// log('2 chop carrots');
+// log('2 chop carrots');
+// log('2 chop carrots');
+// log('2 chop carrots');
 
-function boilWater(time) {
-  log('1 boiling....');
-  setTimeout(() => {
-    // gives the callback function to the web api and the browser takes the callback function. 
-    log('3 done....');
-  }, time);
-}
+// log('I happen first');
+
+// function boilWater(time) {
+//   log('1 boiling....');
+//   setTimeout(() => {
+//     // gives the callback function to the web api and the browser takes the callback function.
+//     log('3 done....');
+//   }, time);
+// }
+
+// setTimeout(function () {
+//   log('I happen third');
+// }, 1000);
+
+// log('I happen second');
+
+// debugger;
+
+// const posts = [
+//   { title: 'Post one', body: 'This is post one' },
+//   { title: 'Post two', body: 'This is post two' },
+// ];
+
+// function getPosts() {
+//   setTimeout(function () {
+//     let output = ''; // output has no value
+
+//     posts.forEach(function (post, index, array) {
+//       output += `<li>${post.body}</li>`;
+//       log(output);
+//       log(array);
+//       document.body.innerHTML = output;
+//     });
+//   }, 1000);
+// }
+
+// // log(getPosts)
+// // getPosts();
+
+// const createPost = function (array, object, callback) {
+//   log(object);
+//   log(callback);
+//   setTimeout(function () {
+//     array.push(object);
+//     callback();
+//   }, 2000);
+// };
+
+// createPost(posts, { title: 'Post three', body: 'This is post three', footer: 'yo' }, getPosts);
+
+// ===== EXERCISE : CALLBACKS =====
+// the callback parameter is a function in this case because it's a callback function
+
+const makeHomeWork = (course, callback) => {
+  setTimeout(function () {
+    log(`Okay I am going to do my ${course} homework now`);
+    callback();
+  }, 1000); // given to the browser and put it on the callstack after 1 sec.
+};
+
+const doneWithHomework = function () {
+  log(`Look mom and dad i'm done with home work`);
+};
+
+makeHomeWork('chemistry', doneWithHomework);
+
+
+const makeHomeWorkSeperated = (course, callback) => {
+    log(`Okay I am going to do my ${course} homework now`);
+    callback(); 
+  // given to the browser and put it on the callstack after 1 sec.
+};
+
+const timeOut = setTimeout(makeHomeWorkSeperated, 5000, 'math', doneWithHomework);
+log(timeOut) // output an ID.
+
+clearTimeout(2) // u can put in id
+setTimeout(timeOut); // u can put in the variable with the value of the setTimeOutID
+
+// it's a reference to the function and don't invoke the function
+
+//You can't get a return value from the function you pass to setTimeout . 
+// The function that called setTimeout(x in your example) will finish executing and return before the function you pass to setTimeout is even called.
+
+
